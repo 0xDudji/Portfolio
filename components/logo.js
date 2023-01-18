e17fd08374faa4655bb1ade61b4769bc7d47655c
Skemil <r0854748@student.thomasmore.be>
2023-01-18 18:18:53 +0100

    Page update

--- components/logo.js
+++ components/logo.js
@@ -35,7 +35,7 @@ const Logo = () => {
             fontWeight="bold"
             ml={3}
           >
-            Emil Dudayev
+            Emil D.
           </Text>
         </LogoBox>
       </a>
